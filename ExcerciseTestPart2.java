f2f8456dc532e0b7d3461a948e44868cb47ee7b1
nimrahahmed <nimrah.ahmed2013@gmail.com>
2015-01-02 13:39:54 +0500

    Chapter18

---
+++ ExcerciseTestPart2.java
@@ -0,0 +1,24 @@
+package tddpart2;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ExcerciseTestPart2 {  
+
+	@Test public void test(){
+	 WasRun test = new WasRun("wasRunTest");
+	 assertTrue(test.wasRun.equals("null"));  
+	 test.run();
+	 assertFalse(test.wasRun.equals("false"));  
+	 assertTrue(test.wasRun.equals("true"));  
+
+	} 
+	@Test public void testRunning()
+	{
+		TestCaseTest testCaseTest = new TestCaseTest("testRunning");
+		testCaseTest.testRunning();
+	}
+}
+
+
---
+++ WasRun.java
@@ -0,0 +1,14 @@
+package tddpart2;
+
+public class WasRun extends TestCase{
+
+	
+
+	WasRun(String name){
+		super(name);
+		wasRun = "null";
+	}
+	
+	
+
+}
